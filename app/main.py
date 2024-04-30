86961cfa8762b51aeca9163a9ea9beaff2274cec
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2024-04-30 09:39:56 +0300

    renamed the file wsgi to main

--- app/main.py
+++ app/main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return f"Container ID: {socket.gethostname()}"
 
+@app.route('/hello')
+def hello():
+    return "Hello World!"
+
 if __name__ == '__main__':
     app.run(debug=True)
