ee1f4322dc46c4d74695f435884180a215e7cd3c
myles4321 <mylesadebayo@gmail.com>
2024-04-30 09:51:46 +0300

    created the /home route

--- app/main.py
+++ app/main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Container ID: {socket.gethostname()}"
+    return "Homepage"
 
-@app.route('/hello')
-def hello():
-    return "Hello World!"
+@app.route('/home')
+def getname():
+    return f"Hello from server: {socket.gethostname()}"
 
 if __name__ == '__main__':
     app.run(debug=True)
